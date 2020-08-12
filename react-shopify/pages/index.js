4f51b9439b1c1e67c9550a9dcd43912b023e9cad
analogy47 <actweed47@gmail.com>
2020-08-12 12:36:59 -0400

    add layout

--- react-shopify/pages/index.js
+++ react-shopify/pages/index.js
@@ -1,8 +1,21 @@
-import { Page, TextStyle } from '@shopify/polaris';
+import { Page, Layout, EmptyState } from '@shopify/polaris';
+const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
 const Index = () => (
   <Page>
-    <TextStyle variation="positive">React Shopify App</TextStyle>
+    <Layout>
+      {/* <TextStyle variation="positive">React Shopify App</TextStyle> */}
+      <EmptyState
+        heading="Discount your products temporarily"
+        action={{
+          content: 'Select products',
+          onAction: () => console.log('clicked'),
+        }}
+        image={img}
+      >
+        <p>Select products to change their price temporarily.</p>
+      </EmptyState>
+    </Layout>
   </Page>
 );
 
