b628ec52d502240a0e07477b69d7168659995f2e
analogy47 <actweed47@gmail.com>
2020-08-14 09:47:31 -0400

    add apollo

--- react-shopify/server/server.js
+++ react-shopify/server/server.js
@@ -4,6 +4,8 @@ const next = require('next');
 const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
 const dotenv = require('dotenv');
 const { verifyRequest } = require('@shopify/koa-shopify-auth');
+const { default: graphQLProxy } = require('@shopify/koa-shopify-graphql-proxy');
+const { ApiVersion } = require('@shopify/koa-shopify-graphql-proxy');
 const session = require('koa-session');
 
 dotenv.config();
@@ -24,7 +26,7 @@ app.prepare().then(() => {
     createShopifyAuth({
       apiKey: SHOPIFY_API_KEY,
       secret: SHOPIFY_API_SECRET_KEY,
-      scopes: ['read_products'],
+      scopes: ['read_products', 'write_products'],
       afterAuth(ctx) {
         const { shop, accessToken } = ctx.session;
         ctx.cookies.set('shopOrigin', shop, {
@@ -37,6 +39,7 @@ app.prepare().then(() => {
     })
   );
 
+  server.use(graphQLProxy({ version: ApiVersion.July1 }));
   server.use(verifyRequest());
   server.use(async (ctx) => {
     await handle(ctx.req, ctx.res);
